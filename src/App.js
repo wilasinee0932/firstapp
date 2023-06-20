93466b0abaed5c51200920ac84d5fe491e1de25a
wilasinee0932 <wilasinee.su.66@ubu.ac.th>
2023-06-20 12:07:42 +0700

    create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [num, setNum] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="MyContent">
+        <h2>{num}</h2>
+        <button onClick={()=>setNum(num+1)}>เพิ่ม</button>
+        <button onClick={()=>setNum(num-1)}>ลด</button>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Wilasinee Sutantangjai</span>
+        </div>
+    );
+}
+
+export default Footer;
