f3768b06220ef9a39520b3fb98dff1af0ac1f5b3
wilasinee0932 <wilasinee.su.66@ubu.ac.th>
2023-06-27 14:12:49 +0700

    create react app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import { useState } from 'react';
 
-
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function App() {
   const [num, setNum] = useState(0);
+  const [prime, setPrime] = useState("");
+
+function checkPrime(cNum) {
+  if (isPrime(cNum)){
+    setPrime("เป็นจำนวนเฉพาะ");
+  }
+  else{
+    setPrime("ไม่เป็นจำนวนเฉพาะ");
+  }
+}
   return (
     <div className="App">
       <Header />
-      <div className="MyContent">
+      <div className='MyContent'>
         <h2>{num}</h2>
-        <button onClick={()=>setNum(num+1)}>เพิ่ม</button>
-        <button onClick={()=>setNum(num-1)}>ลด</button>
+        <button onClick={() =>{
+          let n = num + 1;
+          setNum(n);
+          checkPrime(n);
+        }}>เพิ่ม</button>
+        <button onClick={() =>{
+          let n = num - 1;
+          setNum(n);
+          checkPrime(n);
+        }}>ลด</button>
+        <h3>{prime}</h3>
       </div>
       <Footer />
     </div>
